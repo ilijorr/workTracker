ca2bf9d45a1f98e192d5385b696559fc5c938181
ilijorr <maliodlinka@gmail.com>
2025-10-15 11:33:29 +0200

    feat: added assignment creation

--- back/src/main/java/praksa/zadatak/service/impl/AssignmentServiceImpl.java
+++ back/src/main/java/praksa/zadatak/service/impl/AssignmentServiceImpl.java
@@ -4,12 +4,45 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import praksa.zadatak.dto.AssignmentDTO;
 import praksa.zadatak.dto.CreateAssignmentRequestDTO;
+import praksa.zadatak.exception.ResourceNotFoundException;
+import praksa.zadatak.mapper.AssignmentMapper;
+import praksa.zadatak.model.Assignment;
+import praksa.zadatak.model.Employee;
+import praksa.zadatak.model.Project;
+import praksa.zadatak.repository.AssignmentRepository;
+import praksa.zadatak.repository.EmployeeRepository;
+import praksa.zadatak.repository.ProjectRepository;
 import praksa.zadatak.service.AssignmentService;
 
 @Service
 @RequiredArgsConstructor
 public class AssignmentServiceImpl implements AssignmentService {
+    private final AssignmentRepository assignmentRepository;
+    private final AssignmentMapper assignmentMapper;
+
+    private final ProjectRepository projectRepository;
+    private final EmployeeRepository employeeRepository;
+
     public AssignmentDTO create(CreateAssignmentRequestDTO request) {
-        return null;
+        Long projectId = request.getProjectId();
+        Long employeeId = request.getEmployeeId();
+        ensureProjectAndEmployeeExist(projectId, employeeId);
+
+        Project project = projectRepository.getReferenceById(projectId);
+        Employee employee = employeeRepository.getReferenceById(employeeId);
+
+        Assignment assignment = new Assignment(employee, project, request.getHourRate());
+        assignment = assignmentRepository.save(assignment);
+        return assignmentMapper.toDTO(assignment);
+    }
+
+    private void ensureProjectAndEmployeeExist(Long projectId, Long employeeId) {
+        if (!projectRepository.existsById(projectId)) {
+            throw new ResourceNotFoundException("Project", "id", projectId);
+        }
+
+        if (!employeeRepository.existsById(employeeId)) {
+            throw new ResourceNotFoundException("Employee", "id", employeeId);
+        }
     }
 }
