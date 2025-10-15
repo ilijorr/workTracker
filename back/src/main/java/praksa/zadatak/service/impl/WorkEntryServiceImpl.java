2f6b70a361152d53df9b8fd62b172496529e8992
ilijorr <maliodlinka@gmail.com>
2025-10-15 10:08:08 +0200

    fix: added missing annotation

--- back/src/main/java/praksa/zadatak/service/impl/WorkEntryServiceImpl.java
+++ back/src/main/java/praksa/zadatak/service/impl/WorkEntryServiceImpl.java
@@ -1,9 +1,11 @@
 package praksa.zadatak.service.impl;
 
+import org.springframework.stereotype.Service;
 import praksa.zadatak.dto.CreateWorkEntryRequestDTO;
 import praksa.zadatak.dto.WorkEntryDTO;
 import praksa.zadatak.service.WorkEntryService;
 
+@Service
 public class WorkEntryServiceImpl implements WorkEntryService {
     public WorkEntryDTO create(CreateWorkEntryRequestDTO request) {
         return null;
