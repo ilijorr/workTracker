ef744e0521bdcb9a9a16f755a92e35adc29756ca
ilijorr <maliodlinka@gmail.com>
2025-10-15 10:09:31 +0200

    feat: user creation without auth

--- back/src/main/java/praksa/zadatak/service/impl/EmployeeServiceImpl.java
+++ back/src/main/java/praksa/zadatak/service/impl/EmployeeServiceImpl.java
@@ -1,15 +1,25 @@
 package praksa.zadatak.service.impl;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import praksa.zadatak.dto.CreateEmployeeRequestDTO;
 import praksa.zadatak.dto.EmployeeDTO;
+import praksa.zadatak.mapper.EmployeeMapper;
+import praksa.zadatak.model.Employee;
+import praksa.zadatak.repository.UserRepository;
 import praksa.zadatak.service.EmployeeService;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeMapper employeeMapper;
+    private final UserRepository userRepository;
+
+    @Transactional
     public EmployeeDTO create(CreateEmployeeRequestDTO request) {
-        return null;
+        Employee employee = employeeMapper.toEntity(request);
+        employee = userRepository.save(employee);
+        return employeeMapper.toDTO(employee);
     }
 }
