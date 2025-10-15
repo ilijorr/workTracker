e8d069a404d98d49b8c0d11197c6db75e0d38171
ilijorr <maliodlinka@gmail.com>
2025-10-15 13:57:11 +0200

    feat: vacation request creation and approval/denial

--- back/src/main/java/praksa/zadatak/service/impl/VacationServiceImpl.java
+++ back/src/main/java/praksa/zadatak/service/impl/VacationServiceImpl.java
@@ -1,20 +1,65 @@
 package praksa.zadatak.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import praksa.zadatak.dto.CreateVacationRequestDTO;
 import praksa.zadatak.dto.VacationDTO;
 import praksa.zadatak.enums.VacationStatus;
+import praksa.zadatak.exception.InvalidDateRangeException;
+import praksa.zadatak.exception.ResourceNotFoundException;
+import praksa.zadatak.mapper.VacationMapper;
+import praksa.zadatak.model.Employee;
+import praksa.zadatak.model.Vacation;
+import praksa.zadatak.repository.EmployeeRepository;
+import praksa.zadatak.repository.VacationRepository;
 import praksa.zadatak.service.VacationService;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class VacationServiceImpl implements VacationService {
+    private final VacationRepository vacationRepository;
+    private final VacationMapper vacationMapper;
+
+    private final EmployeeRepository employeeRepository;
+
     public VacationDTO create(CreateVacationRequestDTO request) {
-        return null;
+        ensureValidRequest(request);
+
+        Employee employee = employeeRepository.getReferenceById(request.getEmployeeId());
+        Vacation vacation = vacationMapper.toEntity(request);
+        vacation.setEmployee(employee);
+        vacation.setStatus(VacationStatus.PENDING);
+
+        vacation = vacationRepository.save(vacation);
+
+        return vacationMapper.toDTO(vacation);
     }
 
     public VacationDTO changeStatus(Long id, VacationStatus status) {
-        return null;
+        try {
+            Vacation vacation = vacationRepository.getReferenceById(id);
+            if (vacation.getStatus() != VacationStatus.PENDING) {
+                throw new IllegalStateException("Can only change the status of pending vacations");
+            }
+            vacation.setStatus(status);
+            vacation = vacationRepository.save(vacation);
+            return vacationMapper.toDTO(vacation);
+        } catch (EntityNotFoundException ex) {
+            throw new ResourceNotFoundException("Vacation", "id", id);
+        }
+    }
+
+    private void ensureValidRequest(CreateVacationRequestDTO request) {
+        Date startDate = request.getStartDate();
+        Date endDate = request.getEndDate();
+        if (startDate.after(endDate)) { throw new InvalidDateRangeException(); }
+
+        Long employeeId = request.getEmployeeId();
+        if (!employeeRepository.existsById(employeeId)) { throw new ResourceNotFoundException(
+                "Employee", "id", employeeId
+        ); }
     }
 }
