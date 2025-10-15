320053e92c702dca407835e33ccf44585dc55bbc
ilijorr <maliodlinka@gmail.com>
2025-10-15 11:04:46 +0200

    feat: basic project crud

--- back/src/main/java/praksa/zadatak/service/impl/ProjectServiceImpl.java
+++ back/src/main/java/praksa/zadatak/service/impl/ProjectServiceImpl.java
@@ -1,25 +1,42 @@
 package praksa.zadatak.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 import praksa.zadatak.dto.CreateProjectRequestDTO;
 import praksa.zadatak.dto.ProjectDTO;
+import praksa.zadatak.exception.ResourceNotFoundException;
+import praksa.zadatak.mapper.ProjectMapper;
+import praksa.zadatak.model.Project;
+import praksa.zadatak.repository.ProjectRepository;
 import praksa.zadatak.service.ProjectService;
 
 import java.util.List;
 
+
 @Service
 @RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
+    private final ProjectMapper projectMapper;
+    private final ProjectRepository projectRepository;
+
     public ProjectDTO create(CreateProjectRequestDTO request) {
-        return null;
+        Project project = projectMapper.toEntity(request);
+        project = projectRepository.save(project);
+        return projectMapper.toDTO(project);
     }
 
     public List<ProjectDTO> get() {
-        return null;
+        List<Project> projects = projectRepository.findAll();
+        return projectMapper.toDTOs(projects);
     }
 
     public ProjectDTO get(Long id) {
-        return null;
+        Project project = projectRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Project", "id", id));
+        return projectMapper.toDTO(project);
+
     }
+
 }
