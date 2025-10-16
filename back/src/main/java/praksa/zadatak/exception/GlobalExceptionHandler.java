206f04cba8bb86d0e9c414e890f85b6091f11deb
ilijorr <maliodlinka@gmail.com>
2025-10-16 10:09:07 +0200

    refactor: cleaner exception handling

--- back/src/main/java/praksa/zadatak/exception/GlobalExceptionHandler.java
+++ back/src/main/java/praksa/zadatak/exception/GlobalExceptionHandler.java
@@ -11,45 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler({
+            InvalidDateRangeException.class,
+            InvalidRequestException.class,
+            ResourceNotFoundException.class,
+            UnsupportedOperationException.class,
+            Exception.class,
+    })
     public ResponseEntity<ErrorResponse> handleException(
             @NotNull Exception exception, HttpServletRequest request
-    ) {
-        return buildErrorResponse(exception, request);
-    }
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFound(
-            @NotNull ResourceNotFoundException exception, HttpServletRequest request
-    ) {
-        return buildErrorResponse(exception, request);
-    }
-
-    @ExceptionHandler(NotAssignedException.class)
-    public ResponseEntity<ErrorResponse> handleNotAssigned(
-            @NotNull NotAssignedException exception, HttpServletRequest request
-    ) {
-        return buildErrorResponse(exception, request);
-    }
-
-    @ExceptionHandler(InvalidDateRangeException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidDateRange(
-            @NotNull InvalidDateRangeException exception, HttpServletRequest request
-    ) {
-        return buildErrorResponse(exception, request);
-    }
-
-    @ExceptionHandler(InvalidRequestException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidRequest(
-            @NotNull InvalidRequestException exception, HttpServletRequest request
-    ) {
-        return buildErrorResponse(exception, request);
-    }
-
-    @ExceptionHandler(UnsupportedOperationException.class)
-    public ResponseEntity<ErrorResponse> handleUnimplementedMethod(
-            @NotNull UnsupportedOperationException exception,
-            HttpServletRequest request
     ) {
         return buildErrorResponse(exception, request);
     }
