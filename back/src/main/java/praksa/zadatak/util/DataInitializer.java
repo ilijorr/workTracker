54dd76ea506dedb47978dfbbd33d1617c1b321f3
ilijorr <maliodlinka@gmail.com>
2025-10-20 10:31:51 +0200

    chore: admin creation on startup

---
+++ back/src/main/java/praksa/zadatak/util/DataInitializer.java
@@ -0,0 +1,29 @@
+package praksa.zadatak.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import praksa.zadatak.enums.UserRole;
+import praksa.zadatak.model.Admin;
+import praksa.zadatak.repository.AdminRepository;
+import praksa.zadatak.repository.BaseUserRepository;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+    private final AdminRepository adminRepository;
+    private final BaseUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (userRepository.existsByUsername("admin")) { return; }
+        Admin admin = new Admin();
+        admin.setUsername("admin");
+        admin.setPassword(passwordEncoder.encode("admin"));
+        admin.setRole(UserRole.ROLE_ADMIN);
+        adminRepository.save(admin);
+    }
+}
