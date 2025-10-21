659b6a9617aa3724e47d973d7425e21a8ee12554
ilijorr <maliodlinka@gmail.com>
2025-10-21 14:39:12 +0200

    feat: added vacationDays field for employees

--- back/src/main/java/praksa/zadatak/model/Employee.java
+++ back/src/main/java/praksa/zadatak/model/Employee.java
@@ -1,5 +1,6 @@
 package praksa.zadatak.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Data;
@@ -11,4 +12,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @DiscriminatorValue("EMPLOYEE")
 public class Employee extends BaseUser {
+    @Column(nullable = false)
+    private int vacationDays = 30;
 }
