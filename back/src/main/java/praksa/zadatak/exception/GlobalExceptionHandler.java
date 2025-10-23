9447eaa52c37461cab76c211f6920f933f85b90b
ilijorr <maliodlinka@gmail.com>
2025-10-23 11:55:53 +0200

    fix: error reponse now takes status codes from a map, instead of taking it from annotation, since most of non-custom exceptions arent annotated

--- back/src/main/java/praksa/zadatak/exception/GlobalExceptionHandler.java
+++ back/src/main/java/praksa/zadatak/exception/GlobalExceptionHandler.java
@@ -4,49 +4,48 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({
-            InvalidDateRangeException.class,
-            InvalidRequestException.class,
-            ResourceNotFoundException.class,
-            UnsupportedOperationException.class,
-            UsernameTakenException.class,
-            AuthorizationDeniedException.class, // TODO: throws 500 instead of 401/403
-            NotVacationAuthorized.class,
-            NotEnoughVacationDaysException.class,
-            Exception.class,
-    })
+    private static final Map<Class<? extends Exception>, HttpStatus> EXCEPTION_STATUS_MAP = Map.of(
+            InvalidDateRangeException.class,      HttpStatus.BAD_REQUEST,
+            InvalidRequestException.class,        HttpStatus.BAD_REQUEST,
+            UnsupportedOperationException.class,  HttpStatus.BAD_REQUEST,
+            NotEnoughVacationDaysException.class, HttpStatus.BAD_REQUEST,
+            BadCredentialsException.class,        HttpStatus.UNAUTHORIZED,
+            AuthorizationDeniedException.class,   HttpStatus.FORBIDDEN,
+            NotVacationAuthorized.class,          HttpStatus.FORBIDDEN,
+            ResourceNotFoundException.class,      HttpStatus.NOT_FOUND,
+            UsernameTakenException.class,         HttpStatus.CONFLICT
+    );
+
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(
             @NotNull Exception exception, HttpServletRequest request
     ) {
-        return buildErrorResponse(exception, request);
+        HttpStatus status = EXCEPTION_STATUS_MAP.getOrDefault(
+                exception.getClass(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+        return buildErrorResponse(exception, request, status);
     }
 
-    private ResponseEntity<ErrorResponse> buildErrorResponse(Exception exception, HttpServletRequest request) {
-        HttpStatus status = extractHttpStatus(exception);
+    private ResponseEntity<ErrorResponse> buildErrorResponse(
+            Exception exception,
+            HttpServletRequest request,
+            HttpStatus status
+    ) {
         ErrorResponse errorResponse = new ErrorResponse(
                 status.value(), exception.getMessage(), request.getRequestURI()
         );
         return new ResponseEntity<>(errorResponse, status);
     }
 
-    private HttpStatus extractHttpStatus(Exception exception) {
-        Class<?> exceptionClass = exception.getClass();
-        while (exceptionClass != null) {
-            ResponseStatus responseStatus = exceptionClass.getAnnotation(ResponseStatus.class);
-            if (responseStatus != null) {
-                return responseStatus.value();
-            }
-            exceptionClass = exceptionClass.getSuperclass();
-        }
-
-        return HttpStatus.INTERNAL_SERVER_ERROR;
-    }
 }
