89f89080d7a02470a0644ca520876c94396effa9
ilijorr <maliodlinka@gmail.com>
2025-10-27 16:07:08 +0100

    feat: employee display for admins

--- front/src/app/features/dashboard/components/admin-dashboard/admin-dashboard.ts
+++ front/src/app/features/dashboard/components/admin-dashboard/admin-dashboard.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {EmployeeService} from '../../../employee/employee-service';
+import {Router} from '@angular/router';
+import {TableConfig} from '../../../../shared/models/table-column';
+import {PageResponse} from '../../../../models/response/PageResponse';
+import {Employee} from '../../../../models/Employee';
+import {DataTableComponent} from '../../../../shared/components/data-table/data-table';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [
+    DataTableComponent
+  ],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css',
 })
-export class AdminDashboard {
+export class AdminDashboard implements OnInit {
+  employeeService: EmployeeService = inject(EmployeeService);
+  router: Router = inject(Router);
+
+  employeeData = signal<PageResponse<Employee>>({
+    content: [],
+    totalElements: 0,
+    totalPages: 0,
+    size: 100,
+    number: 0,
+    first: true,
+    last: true
+  });
+
+  isLoading = signal(false);
+
+  employeeTableConfig: TableConfig = {
+    columns: [
+      {
+        key: 'username', label: 'Username', type: 'link',
+        linkConfig: {
+          routePath: '/employee',
+          idField: 'id',
+          displayField: 'username',
+        }
+      },
+      { key: 'vacationDays', label: 'Vacation Days' },
+    ],
+    striped: true,
+    hover: true,
+  };
+
+  loadEmployees(page: number) {
+    this.isLoading.set(true);
+
+    this.employeeService.getAll(page, 100).subscribe({
+      next: (pageResponse: PageResponse<Employee>) => {
+        this.employeeData.set(pageResponse);
+        this.isLoading.set(false);
+      },
+      error: error => {
+      console.log(error);
+      this.isLoading.set(false);
+      }
+    })
+  }
+
+  ngOnInit() {
+    this.loadEmployees(0);
+  }
+
+  onSort(event: any): void {
+    console.log('onSort executed for event ', event);
+  }
 
 }
