0d06f85feedda0c1f52a2ac33ddeccdfab98593b
ilijorr <maliodlinka@gmail.com>
2025-10-27 13:24:04 +0100

    feat: implemented auth-sensitive navbar

---
+++ front/src/app/shared/components/navbar/navbar.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth-service';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.html',
+  styleUrl: 'navbar.css'
+})
+export class NavbarComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+}
