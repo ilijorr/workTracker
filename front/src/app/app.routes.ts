2d02240110df1b3a2830dd9192d8c67e30d4593e
ilijorr <maliodlinka@gmail.com>
2025-10-27 12:55:52 +0100

    feat: guarded routes and added role-sensitive redirection

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,18 +1,32 @@
 import { Routes } from '@angular/router';
+import {guestGuard} from './core/guards/guest-guard';
+import {authGuard} from './core/guards/auth-guard';
+import {inject} from '@angular/core';
+import {AuthService} from './core/services/auth-service';
 
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./features/auth/components/login/login').then(m => m.Login)
+    loadComponent: () =>
+      import('./features/auth/components/login/login').then(m => m.Login),
+    canActivate: [
+      guestGuard,
+    ],
   },
   {
     path: 'dashboard',
     loadComponent: () =>
-      import('./features/dashboard/components/role-based-dashboard/role-based-dashboard').then(m => m.RoleBasedDashboard)
+      import('./features/dashboard/components/role-based-dashboard/role-based-dashboard').then(m => m.RoleBasedDashboard),
+    canActivate: [
+      authGuard,
+    ],
   },
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: () => {
+      const authService = inject(AuthService)
+      return authService.isAuthenticated() ? 'dashboard' : 'login';
+    },
     pathMatch: 'full'
   }
 ];
